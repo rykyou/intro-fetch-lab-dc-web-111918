ab2ced4945067349a5fa3437c7e910b0ab30b703
rykyou <rkim1214@gmail.com>
2019-01-11 12:59:36 -0500

    start on lab

--- swapi.js
+++ swapi.js
@@ -1,1 +1,51 @@
-// Write your swapi code in this file!
+document.addEventListener("DOMContentLoaded", main)
+
+function main() {
+  grabCrawlButton().addEventListener('click', getOpeningCrawl);
+
+  getPlanetNumber();
+  getPlanet();
+}
+
+
+function grabCrawlButton() {
+  return document.getElementById('crawlBtn');
+}
+
+function getOpeningCrawl() {
+  let crawlDiv = document.getElementById('crawlDiv');
+
+  fetch('https://swapi.co/api/films/1/')
+    .then(res => res.json())
+    .then(json => {
+      crawlDiv.innerText = json["opening_crawl"]
+    });
+}
+
+function grabPlanetForm() {
+  return document.getElementById('planetForm');
+}
+
+function getPlanetNumber() {
+
+  return grabPlanetForm().addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let planetData = document.getElementById('planetData');
+    // console.log(e.target[0].value)
+    fetch(`https://swapi.co/api/planets/${e.target[0].value}/`)
+      .then(result => result.json())
+      .then(json => {
+        planetData.innerText = `
+          Name: ${json['name']}
+          Climate: ${json['climate']}
+        `;
+      })
+  });
+
+}
+
+// function getPlanet() {
+//   // console.log(getPlanetNumber)
+//   console.log(`http://swapi.co/api/planets/${getPlanetNumber()}`)
+// }
